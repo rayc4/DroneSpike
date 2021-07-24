61e00914e379938e2b0f97b3b4caa96503560f06
rayc4 <45674759+rayc4@users.noreply.github.com>
2021-07-23 20:58:49 -0400

    Add base code

---
+++ DroneSpike.py
@@ -0,0 +1,30 @@
+import arcade
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "DroneSpike"
+
+class DroneSpike(arcade.Window):
+    ''' Main application class '''
+    
+    def __init__(self):
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def setup(self):
+        ''' Set up the game here. Call this function to restart the game.'''
+        pass
+
+    def on_draw(self):
+        ''' Render the screen. '''
+        arcade.start_render()
+        # Code to draw the screen goes here
+
+def main():
+    window = DroneSpike()
+    window.setup()
+    arcade.run()
+
+if __name__ == '__main__':
+    main()
